6663aed937eac33506cbc450236c7e06fbcf084c
Chiri Francisco <franchiri00@gmail.com>
2024-06-27 17:33:21 -0300

    vs

--- project/src/components/HomeVideo.jsx
+++ project/src/components/HomeVideo.jsx
@@ -53,15 +53,15 @@ function HomeVideo() {
       <div className="clickToPlayMessage">
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          class="icon icon-tabler icon-tabler-click"
+          className="icon icon-tabler icon-tabler-click"
           width="32"
           height="32"
           viewBox="0 0 24 24"
-          stroke-width="2"
+          strokeWidth="2"
           stroke="rgba(255, 255, 255, 0.209)"
           fill="none"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         >
           <path stroke="none" d="M0 0h24v24H0z" fill="none" />
           <path d="M3 12l3 0" />
