8e9fc591e8eef6df17ca5cb6829ca0bcca6f48a1
Chiri Francisco <franchiri00@gmail.com>
2024-07-02 12:49:58 -0300

    vs

--- project/src/components/MusicItem.jsx
+++ project/src/components/MusicItem.jsx
@@ -1,18 +1,12 @@
-import { Link } from "react-router-dom";
 import "../styles/MusicItem.css";
 
 const MusicItem = ({ disco }) => {
   return (
-    <div className="musicItem">
-      <div className="musicItemImgContainer">
-        <img src={disco.image} alt={disco.title} className="musicItemImg"></img>
-        <Link
-          to={`/ProductoDetalles/${disco.id}`}
-          className="ver-mas musicItemButton"
-        ></Link>
-        <div className="musicItemOverlay">
-          <div className="musicItemTitle">{disco.title}</div>
-        </div>
+    <div className="musicItemContainer">
+      <div className="musicItem">
+        <iframe src={disco.urlSong} seamless>
+          <a href={disco.urlToBuy}>{disco.author}</a>
+        </iframe>
       </div>
     </div>
   );
