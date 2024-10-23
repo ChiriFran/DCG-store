b8b99523593841d51db4838d752f3c8006b3c8ad
Chiri Francisco <franchiri00@gmail.com>
2024-10-23 19:45:20 -0300

    vs

--- project/src/components/Faq.jsx
+++ project/src/components/Faq.jsx
@@ -5,6 +5,7 @@ function Faq() {
         <>
             <div className='faqContainer'>
                 <h1 className='faqTitle'>FAQ</h1>
+                <h2 className='faqSubtitles'>Returns?</h2>
                 <p className='faqTexts'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, facere accusantium eligendi
                     recusandae autem quam magni id commodi sit aperiam! In nihil nesciunt dolor, neque ut inventore.
                     Sapiente, sequi provident?
@@ -12,6 +13,32 @@ function Faq() {
                 <p className='faqTexts'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, facere accusantium eligendi
                     recusandae autem quam magni id commodi sit aperiam! In nihil nesciunt dolor, neque ut inventore.
                     Sapiente, sequi provident?
+                </p>
+                <p className='faqTexts'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, facere accusantium eligendi
+                    recusandae autem quam magni id commodi sit aperiam! In nihil nesciunt dolor, neque ut inventore.
+                    Sapiente, sequi provident?
+                </p>
+
+                <h2 className='faqSubtitles'>Shipping?</h2>
+                <p className='faqTexts'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, facere accusantium eligendi
+                    recusandae autem quam magni id commodi sit aperiam! In nihil nesciunt dolor, neque ut inventore.
+                    Sapiente, sequi provident?
+                </p>
+
+                <h2 className='faqSubtitles'>Payment?</h2>
+                <p className='faqTexts'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, facere accusantium eligendi
+                    recusandae autem qua
+                </p>
+                <ul>
+                    <li>Mercadopago</li>
+                    <li>Credit card, MasterCard, Maestro & Visa(MP)</li>
+                    <li>PayPal & PayPal Express</li>
+                </ul>
+
+                <h2 className='faqSubtitles'>Community?</h2>
+                <p className='faqTexts'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, facere accusantium eligendi
+                    recusandae autem qua Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam animi consequuntur quis et. Nostrum,
+                    labore. Atque maxime repellendus unde velit repellat iure beatae corrupti quis, quisquam officia necessitatibus vero veniam.
                 </p>
             </div>
         </>
