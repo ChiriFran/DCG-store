c2f340fa3eb698eacaaab2505c89b92293132c6f
Chiri Francisco <franchiri00@gmail.com>
2024-10-31 16:24:43 -0300

    vs

--- project/src/components/ItemDestacados.jsx
+++ project/src/components/ItemDestacados.jsx
@@ -32,27 +32,7 @@ const ItemDestacados = ({ producto }) => {
         <div className="card-footer">
           <span className="ntext-title itemDestacadosPrice">
             ${producto.price}
-          </span>{" "}
-          <div className="card-button">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="icon icon-tabler icon-tabler-shopping-cart"
-              width="20"
-              height="20"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="#f4f4f4"
-              fill="none"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-              <path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
-              <path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0"></path>
-              <path d="M17 17h-11v-14h-2"></path>
-              <path d="M6 5l14 1l-1 7h-13"></path>
-            </svg>
-          </div>
+          </span>
         </div>
       </div>
     </>
