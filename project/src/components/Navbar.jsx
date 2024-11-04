3f4266adda87e3808fed597f959ec1c923b4a01b
Chiri Francisco <franchiri00@gmail.com>
2024-11-04 18:02:24 -0300

    vs

--- project/src/components/Navbar.jsx
+++ project/src/components/Navbar.jsx
@@ -57,6 +57,7 @@ const Navbar = () => {
         </ul>
 
         <Link to="/" className="link">
+          <img className="logoImg" src="../../media/logo/logo.svg" alt="DCG" title="DCG" />
           <h1 className="brand">D-troit Class'c Galery</h1>
         </Link>
 
