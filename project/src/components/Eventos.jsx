395025d42aad53f9c339a1e2f92543058ad7fcdc
Chiri Francisco <franchiri00@gmail.com>
2024-11-07 21:45:52 -0300

    vs

--- project/src/components/Eventos.jsx
+++ project/src/components/Eventos.jsx
@@ -115,23 +115,6 @@ const Eventos = () => {
                   <label for="star1"></label>
                 </div>
               </div>
-
-              <div className="pastImgEventos">
-                <img src={evento.image} alt="" />
-                {/* From Uiverse.io by PriyanshuGupta28  */}
-                <div class="EventoRating">
-                  <input value="5" name="rating" id="star5" type="radio"></input>
-                  <label for="star5"></label>
-                  <input value="4" name="rating" id="star4" type="radio"></input>
-                  <label for="star4"></label>
-                  <input value="3" name="rating" id="star3" type="radio"></input>
-                  <label for="star3"></label>
-                  <input value="2" name="rating" id="star2" type="radio"></input>
-                  <label for="star2"></label>
-                  <input value="1" name="rating" id="star1" type="radio"></input>
-                  <label for="star1"></label>
-                </div>
-              </div>
             </div>
           </div>
         ))}
